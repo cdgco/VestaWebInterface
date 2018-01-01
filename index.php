b434df6d657411d2ad54fc186cbc7b96adc01e96
CDG <cdgco@users.noreply.github.com>
2018-01-01 04:40:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
             <link href="css/animate.css" rel="stylesheet">
             <link href="css/style.css" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.5/sweetalert2.css" />
-            <link href="css/colors/***REMOVED*** echo $themecolor; ***REMOVED***.css" id="theme" rel="stylesheet">
+            <link href="css/colors/***REMOVED*** echo $themecolor; ***REMOVED***" id="theme" rel="stylesheet">
             <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
