1572ab1363363f4f7612ba5dabb4cbf03ac857d3
CDG <cdgco@users.noreply.github.com>
2018-01-01 05:35:05 -0800

    Rename config.php to config-example.php

---
+++ includes/config-example.php
@@ -0,0 +1,150 @@
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+DEFINE('SITE_NAME', 'My Host'); // Site name for use in page titles. Ex: 'My Host Company'.
+***REMOVED***
+
+***REMOVED***
+DEFINE('VESTA_HOST_ADDRESS', 'myhost.com'); // URL or IP Address of VestaCP. Ex: 'myhost.com' or '12.34.56.78'.
+***REMOVED***
+DEFINE('VESTA_PORT', ''); // VestaCP port. Leave blank for VestCP default. Ex: '8083'.
+***REMOVED***
+DEFINE('VESTA_ADMIN_PW', ''); // Password for VestaCP Admin account. Ex: 'MyPassword1'.
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+DEFINE('PHPPGADMIN_URL', 'disabled'); // phpPgAdmin URL. Leave blank for VestaCP default. Set as 'disabled; to remove phpPgAdmin option.
+DEFINE('SUPPORT_URL', 'disabled'); // Support URL. Leave blank or set to 'disabled' to disable.
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***/////////////
+***REMOVED***
+***REMOVED***/////////////
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+$uname = base64_decode($_COOKIE['username']);
+$loggedin = base64_decode($_COOKIE['loggedin']);
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+    $phpmyadmin = $vst_ssl . VESTA_HOST_ADDRESS . '/phppgadmin';
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+***REMOVED***
+   ***REMOVED***
+***REMOVED***
+
+include 'functions.php'
+***REMOVED***
