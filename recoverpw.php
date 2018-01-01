5257ffd4dda95a61d52ca805ca5e3af6acf9a7ff
CDG <cdgco@users.noreply.github.com>
2018-01-01 06:44:57 -0800

    Remove custom.min.js

--- recoverpw.php
+++ recoverpw.php
@@ -95,7 +95,7 @@ $confirm = "block";
 <!--Wave Effects -->
 <script src="js/waves.js"></script>
 <!-- Custom Theme JavaScript -->
-<script src="js/custom.min.js"></script>
+<script src="js/custom.js"></script>
 <!--Style Switcher -->
 <script src="plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
 </body>
