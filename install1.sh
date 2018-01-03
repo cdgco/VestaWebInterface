2ee710498a4daa3a39e75c15e3d9c533b2483e8a
CDG <cdgco@users.noreply.github.com>
2018-01-03 00:32:31 -0800

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Linux
+wget=/usr/bin/wget
+tar=/bin/tar
+
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+elif [ ! -x "$wget" ]; then
+  echo "ERROR: No wget." >&2
+  exit 1
+elif [ ! -x "$tar" ]; then
+  echo "ERROR: No tar." >&2
+  exit 1
+fi
+printf "\n"
+PKG_OK=$(dpkg-query -W --showformat='${Status***REMOVED***\n' git|grep "install ok installed")
+echo Checking for GIT: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "Git not found. Setting up Git."
+  sudo apt-get --force-yes --yes install git
+else
+  printf "Git is already installed.\nMoving on ...\n"
+fi
+printf "\n"
+printf "Installing Vesta Web Interface Backend ..."
+printf "\n"
+git clone https://github.com/cdgco/VestaWebInterface .
+printf '\n'
+sleep .5
+wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xz -C /usr/local/vesta/web
+printf "\n"
+printf "Installation Complete!"
+printf "\n"
