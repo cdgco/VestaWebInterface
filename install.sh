8d220897cb342663539028b4d3a8a3209997bdc9
CDG <cdgco@users.noreply.github.com>
2018-01-03 00:05:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,17 @@ elif [ ! -x "$tar" ]; then
   exit 1
 fi
 
-wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xvz -C /usr/local/vesta/web
+echo "
 
-fi
+ __   __      _         __      __   _      ___     _            __             
+ \ \ / /__ __| |_ __ _  \ \    / /__| |__  |_ _|_ _| |_ ___ _ _ / _|__ _ __ ___ 
+  \ V / -_|_-<  _/ _` |  \ \/\/ / -_) '_ \  | || ' \  _/ -_) '_|  _/ _` / _/ -_)
+   \_/\___/__/\__\__,_|   \_/\_/\___|_.__/ |___|_||_\__\___|_| |_| \__,_\__\___|
+   
+   "
+sleep .5
+printf "Installing VWI Backend ..."
+sleep .5
+wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xvz -C /usr/local/vesta/web
+printf "\n"
+printf "Installation Complete!"
