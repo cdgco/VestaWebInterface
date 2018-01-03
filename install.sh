e5efe903e316fb80397a529edaf0fd5617cf4138
CDG <cdgco@users.noreply.github.com>
2018-01-03 00:07:29 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,11 @@ elif [ ! -x "$tar" ]; then
   echo "ERROR: No tar." >&2
   exit 1
 fi
+printf "\n"
 printf "Installing Vesta Web Interface Backend ..."
+printf "\n"
 sleep .5
 wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xvz -C /usr/local/vesta/web
 printf "\n"
 printf "Installation Complete!"
+printf "\n"
