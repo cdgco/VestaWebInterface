b519ed7fd138f8a74ae9f368d7c0afd0cd942769
CDG <cdgco@users.noreply.github.com>
2018-01-03 00:06:37 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,17 +14,7 @@ elif [ ! -x "$tar" ]; then
   echo "ERROR: No tar." >&2
   exit 1
 fi
-
-echo "
-
- __   __      _         __      __   _      ___     _            __             
- \ \ / /__ __| |_ __ _  \ \    / /__| |__  |_ _|_ _| |_ ___ _ _ / _|__ _ __ ___ 
-  \ V / -_|_-<  _/ _` |  \ \/\/ / -_) '_ \  | || ' \  _/ -_) '_|  _/ _` / _/ -_)
-   \_/\___/__/\__\__,_|   \_/\_/\___|_.__/ |___|_||_\__\___|_| |_| \__,_\__\___|
-   
-   "
-sleep .5
-printf "Installing VWI Backend ..."
+printf "Installing Vesta Web Interface Backend ..."
 sleep .5
 wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xvz -C /usr/local/vesta/web
 printf "\n"
