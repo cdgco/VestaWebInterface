e7b3aa2207226a14c6a8fa4c8c93ab6f66b2c317
CDG <cdgco@users.noreply.github.com>
2018-01-02 23:49:45 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
-#!/bin/bash
+#!/bin/sh
+
+# Linux
+wget=/usr/bin/wget
+tar=/bin/tar
 
 if [ "x$(id -u)" != 'x0' ]; then
     echo 'Error: this script can only be executed by root'
     exit 1
+elif [ ! -x "$wget" ]; then
+  echo "ERROR: No wget." >&2
+  exit 1
+elif [ ! -x "$tar" ]; then
+  echo "ERROR: No tar." >&2
+  exit 1
 fi
 
-function InstallVestaWebInterface()
-{
-	echo "Installing Vesta Web Interface Backend ..."
-	
-	wget https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
-	
-        tar xf web.tar.gz --overwrite -C /usr/local/vesta/web 
-	
-	fi
+if ! cd "/usr/local/vesta/web"; then
+  echo "ERROR: can't access working directory (/usr/local/vesta/web)" >&2
+  exit 1
+fi
 
-	echo "Done!";
-***REMOVED***
-InstallVestaWebInterface
+if ! wget "https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz"; then
+  echo "ERROR: can't get archive" >&2
+  exit 1
+fi
+
+# extract archive
+if [ ! tar xf web.tar.gz --overwrite -C /usr/local/vesta/web ]; then
+  echo "ERROR: Archive not found" >&2
+  exit 1
+fi
+
+done
