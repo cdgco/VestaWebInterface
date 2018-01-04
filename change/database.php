8470b52105d343a10c1a2b66801e7b7fce73544b
CDG <cdgco@users.noreply.github.com>
2018-01-03 21:06:54 -0800

    Bug Fixes

--- change/database.php
+++ change/database.php
@@ -7,18 +7,11 @@
     $v_1 = $_POST['v_database'];
     $v_2 = $_POST['v_dbuser'];
     $v_3 = $_POST['password'];
-    $v_4 = $_POST['v_type'];
-    $v_5 = $_POST['v_host'];
-    $v_6 = $_POST['v_charset'];
 
     if ((!isset($_POST['v_database'])) || ($_POST['v_database'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
-    elseif ((!isset($_POST['v_dbuser'])) || ($_POST['v_dbuser'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
-    elseif ((!isset($_POST['password'])) || ($_POST['password'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
-    elseif ((!isset($_POST['v_type'])) || ($_POST['v_type'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
-    elseif ((!isset($_POST['v_host'])) || ($_POST['v_host'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
-    elseif ((!isset($_POST['v_charset'])) || ($_POST['v_charset'] == '')) { header('Location: ../list/db.php?returncode=1');***REMOVED***
+    elseif ((!isset($_POST['v_dbuser'])) || ($_POST['v_dbuser'] == '')) { header('Location: ../edit/db.php?returncode=1&domain=' . $v_1);***REMOVED***
 
-    $postvars = array('user' => $vst_username,'password' => $vst_password,'returncode' => 'yes','cmd' => 'v-add-database','arg1' => $username,'arg2' => $v_1, 'arg3' => $v_2, 'arg4' => $v_3, 'arg5' => $v_4, 'arg6' => $v_5, 'arg7' => $v_6);
+    $postvars = array('user' => $vst_username,'password' => $vst_password,'returncode' => 'yes','cmd' => 'v-change-database-user','arg1' => $username,'arg2' => $v_1, 'arg3' => $v_2, 'arg4' => $v_3);
 
     $curl0 = curl_init();
     curl_setopt($curl0, CURLOPT_URL, $vst_url);
@@ -29,6 +22,6 @@
     curl_setopt($curl0, CURLOPT_POSTFIELDS, http_build_query($postvars));
     $r1 = curl_exec($curl0);
 
-    header('Location: ../list/db.php?returncode=' . $r1);
+    header('Location: ../edit/db.php?returncode=' . $r1 . '&domain=' . $v_1);
 
 ***REMOVED***
