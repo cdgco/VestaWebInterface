06a7f48aa7aa83001084cd6bd91620667f843aaf
Carter Roeser <carter@cdgtech.one>
2018-01-06 09:31:29 -0800

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -2,7 +2,6 @@
 
 if [ "x$(id -u)" != 'x0' ]; then
 	echo 'Error: this script can only be executed by root'
-
 	exit 1
 elif [ -f /etc/os-release ]; then
     . /etc/os-release
@@ -32,9 +31,7 @@ if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then
 		apt-get install git;
 	fi
 	printf "\nInstalling Vesta Web Interface frontend ...\n"
-	
-git clone --quiet https://github.com/cdgco/VestaWebInterface .
- > /dev/null
+	git clone --quiet https://github.com/cdgco/VestaWebInterface . > /dev/null
 	if [ -f install1.sh ] ; then
 		rm install1.sh
 	elif [ -f install2.sh ] ; then
@@ -46,19 +43,14 @@ git clone --quiet https://github.com/cdgco/VestaWebInterface .
 	elif [ -d includes ] ; then
 		chmod 777 includes	
 	fi
-	
-printf "Installing Vesta Web Interface backend ...\n"
-	
-sleep .5
-
+	printf "Installing Vesta Web Interface backend ...\n"
+	sleep .5
 	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
 	if [ -f web.tar.gz ] ; then
 		tar -xzf web.tar.gz -C /usr/local/vesta/web
-
 		rm web.tar.gz
 	fi
 	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
-
 elif [ $OS == "CentOS Linux" ] || [ $OS == "RHEL" ]; then
 	if [ ! -z "$(ls -A ./)" ]; then
 		printf "Error: Directory not empty.\nVWI must be installed in clean directory. Exiting ...\n"
@@ -70,10 +62,8 @@ elif [ $OS == "CentOS Linux" ] || [ $OS == "RHEL" ]; then
 	elif [ ! yum list installed git >/dev/null 2>&1 ]; then
 		yum -y install git
 	fi
-	printf "\nInstalling Vesta Web Interface frontend ...\n"
-	
-git clone --quiet https://github.com/cdgco/VestaWebInterface .
- > /dev/null
+	printf "\nInstalling Vesta Web Interface frontend ...\n"	
+	git clone --quiet https://github.com/cdgco/VestaWebInterface . > /dev/null
 	if [ -f install1.sh ] ; then
 		rm install1.sh
 	elif [ -f install2.sh ] ; then
@@ -84,20 +74,15 @@ git clone --quiet https://github.com/cdgco/VestaWebInterface .
 		rm 'VWI Banner.png'
 	elif [ -d includes ] ; then
 		chmod 777 includes	
-	fi
-	
-printf "Installing Vesta Web Interface backend ...\n"
-	
-sleep .5
-
+	fi	
+	printf "Installing Vesta Web Interface backend ...\n"	
+	sleep .5
 	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
 	if [ -f web.tar.gz ] ; then
 		tar -xzf web.tar.gz -C /usr/local/vesta/web
-
 		rm web.tar.gz
 	fi
 	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
-
 else
 	echo 'Error: VWI can only be installed on Debian, Ubuntu, CentOS, or RHEL. Exiting ...'
 	exit 1
