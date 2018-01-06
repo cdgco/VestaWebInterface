48a54310b15f9208fc3b323cb3a223d2764f62e4
Carter Roeser <carter@cdgtech.one>
2018-01-06 09:29:30 -0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,24 +1,73 @@
-#!/bin/bash
-
-# Linux
-wget=/usr/bin/wget
-tar=/bin/tar
 
 if [ "x$(id -u)" != 'x0' ]; then
-    echo 'Error: this script can only be executed by root'
-    exit 1
-elif [ ! -x "$wget" ]; then
-  echo "ERROR: No wget." >&2
-  exit 1
-elif [ ! -x "$tar" ]; then
-  echo "ERROR: No tar." >&2
-  exit 1
+	echo 'Error: this script can only be executed by root'
+
+	exit 1
+elif [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    OS=Debian
+elif [ -f /etc/redhat-release ]; then
+    OS=RHEL
+else
+    OS=$(uname -s)
 fi
-printf "\n"
-printf "Installing Vesta Web Interface Backend ..."
-printf "\n"
+
+if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then 
+	if [ ! -z "$(ls -A ./)" ]; then
+		printf "Error: Directory not empty.\nVWI must be installed in clean directory. Exiting ...\n"
+		exit 1
+	elif [ $(dpkg-query -W -f='${Status***REMOVED***' wget 2>/dev/null | grep -c "tar found") -eq 1 ]; then
+		apt-get install wget;
+	elif [ $(dpkg-query -W -f='${Status***REMOVED***' tar 2>/dev/null | grep -c "tar found") -eq 1 ]; then
+		apt-get install tar;
+	elif [ $(dpkg-query -W -f='${Status***REMOVED***' git 2>/dev/null | grep -c "git found") -eq 1 ]; then
+		apt-get install git;
+	fi
+	
+printf "Installing Vesta Web Interface backend ...\n"
+	
 sleep .5
-wget -qO- https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz | tar xz -C /usr/local/vesta/web
-printf "\n"
-printf "Backend Installation Complete! Please visit your website online to finish configuration."
-printf "\n"
+
+	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
+	if [ -f web.tar.gz ] ; then
+		tar -xzf web.tar.gz -C /usr/local/vesta/web
+
+		rm web.tar.gz
+	fi
+	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
+
+elif [ $OS == "CentOS Linux" ] || [ $OS == "RHEL" ]; then
+	if [ ! -z "$(ls -A ./)" ]; then
+		printf "Error: Directory not empty.\nVWI must be installed in clean directory. Exiting ...\n"
+		exit 1
+	elif [ ! yum list installed wget >/dev/null 2>&1 ]; then
+		yum -y install wget
+	elif [ ! yum list installed tar >/dev/null 2>&1 ]; then
+		yum -y install tar
+	elif [ ! yum list installed git >/dev/null 2>&1 ]; then
+		yum -y install git
+	fi
+	
+printf "Installing Vesta Web Interface backend ...\n"
+	
+sleep .5
+
+	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
+	if [ -f web.tar.gz ] ; then
+		tar -xzf web.tar.gz -C /usr/local/vesta/web
+
+		rm web.tar.gz
+	fi
+	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
+
+else
+	echo 'Error: VWI can only be installed on Debian, Ubuntu, CentOS, or RHEL. Exiting ...'
+	exit 1
+fi
