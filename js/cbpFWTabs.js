58c66514fbd26e53607a723e4be1b0e127125746
Carter Roeser <carter@cdgtech.one>
2018-01-06 14:43:42 -0800

    Update cbpFWTabs.js

--- js/cbpFWTabs.js
+++ js/cbpFWTabs.js
@@ -10,7 +10,7 @@
  */
 ( function( window ) {
 	
-	'use strict';
+	"use strict";
 
 	function extend( a, b ) {
 		for( var key in b ) { 
@@ -24,8 +24,8 @@
 	function CBPFWTabs( el, options ) {
 		this.el = el;
 		this.options = extend( {***REMOVED***, this.options );
-  		extend( this.options, options );
-  		this._init();
+		extend( this.options, options );
+		this._init();
 	***REMOVED***
 
 	CBPFWTabs.prototype.options = {
@@ -34,9 +34,9 @@
 
 	CBPFWTabs.prototype._init = function() {
 		// tabs elems
-		this.tabs = [].slice.call( this.el.querySelectorAll( 'nav > ul > li' ) );
+		this.tabs = [].slice.call( this.el.querySelectorAll( "nav > ul > li" ) );
 		// content items
-		this.items = [].slice.call( this.el.querySelectorAll( '.content-wrap > section' ) );
+		this.items = [].slice.call( this.el.querySelectorAll( ".content-wrap > section" ) );
 		// current index
 		this.current = -1;
 		// show current content item
@@ -48,7 +48,7 @@
 	CBPFWTabs.prototype._initEvents = function() {
 		var self = this;
 		this.tabs.forEach( function( tab, idx ) {
-			tab.addEventListener( 'click', function( ev ) {
+			tab.addEventListener( "click", function( ev ) {
 				ev.preventDefault();
 				self._show( idx );
 			***REMOVED*** );
@@ -57,12 +57,12 @@
 
 	CBPFWTabs.prototype._show = function( idx ) {
 		if( this.current >= 0 ) {
-			this.tabs[ this.current ].className = this.items[ this.current ].className = '';
+			this.tabs[ this.current ].className = this.items[ this.current ].className = "";
 		***REMOVED***
 		// change current
-		this.current = idx != undefined ? idx : this.options.start >= 0 && this.options.start < this.items.length ? this.options.start : 0;
-		this.tabs[ this.current ].className = 'tab-current';
-		this.items[ this.current ].className = 'content-current';
+		this.current = idx !== undefined ? idx : this.options.start >= 0 && this.options.start < this.items.length ? this.options.start : 0;
+		this.tabs[ this.current ].className = "tab-current";
+		this.items[ this.current ].className = "content-current";
 	***REMOVED***;
 
 	// add to global namespace
