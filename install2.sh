b1df574119e03585f021d004fef532f22ab3f102
Carter Roeser <carter@cdgtech.one>
2018-01-06 09:33:34 -0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 
 if [ "x$(id -u)" != 'x0' ]; then
 	echo 'Error: this script can only be executed by root'
-
 	exit 1
 elif [ -f /etc/os-release ]; then
     . /etc/os-release
@@ -18,8 +18,7 @@ elif [ -f /etc/redhat-release ]; then
 else
     OS=$(uname -s)
 fi
-
-if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then 
+if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then
 	if [ ! -z "$(ls -A ./)" ]; then
 		printf "Error: Directory not empty.\nVWI must be installed in clean directory. Exiting ...\n"
 		exit 1
@@ -30,19 +29,14 @@ if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then
 	elif [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "git found") -eq 1 ]; then
 		apt-get install git;
 	fi
-	
-printf "Installing Vesta Web Interface backend ...\n"
-	
-sleep .5
-
+	printf "Installing Vesta Web Interface backend ...\n"
+	sleep .5
 	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
 	if [ -f web.tar.gz ] ; then
 		tar -xzf web.tar.gz -C /usr/local/vesta/web
-
 		rm web.tar.gz
 	fi
 	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
-
 elif [ $OS == "CentOS Linux" ] || [ $OS == "RHEL" ]; then
 	if [ ! -z "$(ls -A ./)" ]; then
 		printf "Error: Directory not empty.\nVWI must be installed in clean directory. Exiting ...\n"
@@ -54,19 +48,14 @@ elif [ $OS == "CentOS Linux" ] || [ $OS == "RHEL" ]; then
 	elif [ ! yum list installed git >/dev/null 2>&1 ]; then
 		yum -y install git
 	fi
-	
-printf "Installing Vesta Web Interface backend ...\n"
-	
-sleep .5
-
+	printf "Installing Vesta Web Interface backend ...\n"
+	sleep .5
 	wget -q https://raw.githubusercontent.com/cdgco/VestaWebInterface/master/install/web.tar.gz
 	if [ -f web.tar.gz ] ; then
 		tar -xzf web.tar.gz -C /usr/local/vesta/web
-
 		rm web.tar.gz
 	fi
 	printf "\nInstallation Complete! Please visit your website online to finish configuration.\n"
-
 else
 	echo 'Error: VWI can only be installed on Debian, Ubuntu, CentOS, or RHEL. Exiting ...'
 	exit 1
