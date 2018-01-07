f4c5775c1d5d64d6436884819121c5694a79f2ef
Carter Roeser <carter@cdgtech.one>
2018-01-06 18:31:16 -0800

    Update theme.php

--- process/theme.php
+++ process/theme.php
@@ -1,8 +1,8 @@
-<***REMOVED***
+***REMOVED***
 
 session_start();
 
-$cookie1 = base64_encode($_GET['theme'] . '.css');
-setcookie('theme', $cookie1, time() + (86400 * 30), "/");
+$_SESSION["theme"] = base64_encode($_GET["theme"] . ".css");
+header("Location: ../index.php");
 
 ***REMOVED***
