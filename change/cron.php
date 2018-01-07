784e05c1c4f30fcbe8edd536a997af1c0671ac1e
Carter Roeser <carter@cdgtech.one>
2018-01-06 16:42:53 -0800

    Update cron.php

--- change/cron.php
+++ change/cron.php
@@ -1,24 +1,23 @@
 <?php
 
     if (file_exists( '../includes/config.php' )) { require( '../includes/config.php'); }  else { header( 'Location: ../install' );};
-    if(base64_decode($_COOKIE['loggedin']) == 'true') {}
-    else { header('Location: ../login.php'); }
+    if(base64_decode($_COOKIE['loggedin']) != 'true') { header('Location: ../login.php'); }
 
-    $v_min = $_POST['v_min'];
-    $v_hour = $_POST['v_hour'];
-    $v_day = $_POST['v_day'];
-    $v_month = $_POST['v_month'];
-    $v_wday = $_POST['v_wday'];
-    $v_cmd = $_POST['v_cmd'];
-    $v_job = $_POST['v_job'];
-
-    if ((!isset($_POST['v_min'])) || ($_POST['v_min'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_hour'])) || ($_POST['v_hour'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_day'])) || ($_POST['v_day'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_month'])) || ($_POST['v_month'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_wday'])) || ($_POST['v_wday'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_cmd'])) || ($_POST['v_cmd'] == '')) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
-    elseif ((!isset($_POST['v_job'])) || ($_POST['v_job'] == '')) { header('Location: ../list/cron.php?returncode=1');}
+    if(isset($_POST['v_min']) { $v_min = $_POST['v_min']; }
+    if(isset($_POST['v_hour']) { $v_hour = $_POST['v_hour']; }
+    if(isset($_POST['v_day']) { $v_month = $_POST['v_month']; }
+    if(isset($_POST['v_month']) { $v_month = $_POST['v_month']; }
+    if(isset($_POST['v_wday']) { $v_wday = $_POST['v_wday']; }
+    if(isset($_POST['v_cmd']) { $v_cmd = $_POST['v_cmd']; }
+    if(isset($_POST['v_job']) { $v_job = $_POST['v_job']; }
+       
+    if (!isset($v_min)) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_hour)) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_day) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_month)) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_wday)) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_cmd)) { header('Location: ../edit/cron.php?returncode=1&job=' . $v_job);}
+    elseif (!isset($v_job)) { header('Location: ../list/cron.php?returncode=1');}
 
     $postvars = array('user' => $vst_username,'password' => $vst_password,'returncode' => 'yes','cmd' => 'v-change-cron-job','arg1' => $username,'arg2' => $v_job, 'arg3' => $v_min, 'arg4' => $v_hour, 'arg5' => $v_day, 'arg6' => $v_month, 'arg7' => $v_wday, 'arg8' => $v_cmd);
 
