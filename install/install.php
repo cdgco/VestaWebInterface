7defe05ae50b96170fcc58b6f0941d355215fbab
CDG <cdgco@users.noreply.github.com>
2018-01-10 21:13:49 -0800

    Bug Fixes

--- install/install.php
+++ install/install.php
@@ -14,13 +14,12 @@ else { $dbenabled = 'false'; ***REMOVED***
 if($_POST['ENABLE_OLDCPURL'] == 'on'){ $oldcplink = 'true'; ***REMOVED***
 else { $oldcplink = 'false'; ***REMOVED***
 
-require('../plugins/bower_components/woopra/track.php');
+require('../includes/tracker.php');
 $woopra = new WoopraTracker(array("domain" => 'vwi-install.tracker'));
 $woopra->set_woopra_cookie();
 $woopra->identify(array(
 "name" => $_POST['SITENAME'],
-"url" => substr( "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 0, -19)
-));
+"url" => substr( "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 0, -19)));
 
 
 $writestr = "***REMOVED***
@@ -171,13 +170,12 @@ DEFINE('INTERAKT_API_KEY', '".$_POST['INTERAKT_API_KEY']."'); // Enable Interakt
  \$oldcpurl = \$url8083;
 ***REMOVED***
 ***REMOVED***
-require('../plugins/bower_components/woopra/track.php');
+require('tracker.php');
 \$woopra = new WoopraTracker(array('domain' => 'vwi-install.tracker'));
 \$woopra->set_woopra_cookie();
 \$woopra->identify(array(
 'sitename' => \$sitename,
-'url' => \$_SERVER[HTTP_HOST] . \$_SERVER[REQUEST_URI];
-));
+'url' => \$_SERVER[HTTP_HOST] . \$_SERVER[REQUEST_URI]));
 
 ***REMOVED***";
 
