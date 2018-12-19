a77e3a07f4ce19868d6e0ac7c87111ca24713bf6
Carter Roeser <carter@cdgtech.one>
2018-12-18 17:02:31 -0800

    Dependencies

--- plugins/components/bootstrap-select/js/i18n/defaults-hr_HR.js
+++ plugins/components/bootstrap-select/js/i18n/defaults-hr_HR.js
@@ -1,11 +1,12 @@
 /*!
- * Bootstrap-select v1.13.1 (https://developer.snapappointments.com/bootstrap-select)
+ * Bootstrap-select v1.13.5 (https://developer.snapappointments.com/bootstrap-select)
  *
  * Copyright 2012-2018 SnapAppointments, LLC
  * Licensed under MIT (https://github.com/snapappointments/bootstrap-select/blob/master/LICENSE)
  */
 
 (function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["jquery"], function (a0) {
@@ -26,7 +27,7 @@
     noneSelectedText: 'Odaberite stavku',
     noneResultsText: 'Nema rezultata pretrage {0}',
     countSelectedText: function (numSelected, numTotal) {
-      return (numSelected == 1) ? "{0} stavka selektirana" : "{0} stavke selektirane";
+      return (numSelected == 1) ? '{0} stavka selektirana' : '{0} stavke selektirane';
     },
     maxOptionsText: function (numAll, numGroup) {
       return [
