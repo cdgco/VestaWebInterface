cf35f8f77b6adaa8d656d4929150bcfd662b24da
Carter Roeser <carter@cdgtech.one>
2020-04-13 14:53:53 -0700

    Create upgrade.sql

---
+++ install/upgrade.sql
@@ -0,0 +1,31 @@
+/** 
+*
+* Vesta Web Interface
+*
+* Copyright (C) 2020 Carter Roeser <carter@cdgtech.one>
+* https://cdgco.github.io/VestaWebInterface
+*
+* Vesta Web Interface is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as published 
+* by the Free Software Foundation.
+*
+* Vesta Web Interface is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Vesta Web Interface.  If not, see
+* <https://github.com/cdgco/VestaWebInterface/blob/master/LICENSE>.
+*
+*/
+
+/*
+* Table structure for table `vwi_auth0-users`
+*/
+
+CREATE TABLE IF NOT EXISTS `vwi_auth0-users` (
+  `VWI_USER` varchar(64) NOT NULL,
+  `AUTH0_USER` varchar(1024) NOT NULL
+  PRIMARY KEY (`VWI_USER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
